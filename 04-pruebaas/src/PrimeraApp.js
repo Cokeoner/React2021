286e7b468333dd91972ff62029b8b36ebbd43c8e
MIGUEL ANGEL MARTINEZ FLORES <cokeoner1995@gmail.com>
2021-07-04 04:04:27 -0500

    Se hace el commit de la version de la actividad 60 de la session 5, de pruebas con Enzyme

--- 04-pruebaas/src/PrimeraApp.js
+++ 04-pruebaas/src/PrimeraApp.js
@@ -6,7 +6,7 @@ const PrimeraApp = ({ saludo, subtitulo }) => {
     return  (
         <>
             <h1> { saludo } </h1>
-            <p> { subtitulo } </p>
+            <p>{ subtitulo }</p>
         </>
     );
 };
--- 04-pruebaas/src/tests/PrimeraApp.test.js
+++ 04-pruebaas/src/tests/PrimeraApp.test.js
@@ -1,18 +1,43 @@
-import { render } from "@testing-library/react";
-import PrimeraApp from  "../PrimeraApp";
 import React from 'react';
+import { shallow } from 'enzyme'
+// import { render } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import PrimeraApp from  "../PrimeraApp";
 
 describe('Pruebbas PrimeraApp', () => {
     
-    test('Debe de mostrar el mensaje "Hola soy goku"', () => {
-        
-        const saludo = 'Hola soy goku';
-        
-        const {getByText} = render(<PrimeraApp saludo= { saludo }/>);
+    // test('Debe de mostrar el mensaje "Hola soy goku"', () => {  
+    //     const saludo = 'Hola soy goku';
+    //     const {getByText} = render(<PrimeraApp saludo= { saludo }/>);
+    //     expect( getByText(saludo)).toBeInTheDocument();
+    // });
 
-        expect( getByText(saludo)).toBeInTheDocument();
+    test('Debe mostrar <PrimeraApp /> correctamente', () => {
+       
+        const saludo = 'Hola soy goku';
+        const Wrapper = shallow( <PrimeraApp saludo= { saludo }/>)
+
+        expect( Wrapper).toMatchSnapshot();
 
     });
+    
+    test('Dee de mostrar el susbstitulo enviado por props', () => {
+        
+        const saludo = 'Hola soy goku';
+        const subtitulo = 'Soy un Subtitulo';
+        const Wrapper = shallow( 
+                <PrimeraApp 
+                        saludo= { saludo }
+                        subtitulo ={ subtitulo} 
+                
+                />)
+
+        const TextoParrado = Wrapper.find('p').text();
+
+        expect( TextoParrado).toBe(subtitulo);
+
+    })
+    
     
 
 });
