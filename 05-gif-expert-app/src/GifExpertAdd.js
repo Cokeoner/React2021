5adc98c9f878e26d57ce956e71cfc2d8b2425fd3
MIGUEL ANGEL MARTINEZ FLORES <cokeoner1995@gmail.com>
2021-07-04 21:51:53 -0500

    Se hace el commit de los cambios que se han generado en la clase numero 69 es la seccion 6

---
+++ 05-gif-expert-app/src/GifExpertAdd.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const GifExpertAdd = () => {
+    return (
+        <div>
+            <h2>GifExpertApp</h2>
+            <hr/>
+        </div>
+    )
+}
+
+export default GifExpertAdd;
