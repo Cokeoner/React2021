77773cd100335c037e9d60cbe2891872d27d4c96
cokeoner <cokeoner1995@gmail.com>
2021-07-08 17:26:17 -0500

    se genera el comit de la clase 89, de la secccion 8

--- 06-gif-expert-app-build/src/test/components/GifGridItem.test.js
+++ 06-gif-expert-app-build/src/test/components/GifGridItem.test.js
@@ -1,5 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { GifGridItem } from '../../components/GifGridItem';
 
 describe( 'Pruebas en <GridGridItem />', () => {
 
-    
+    const title = 'Un titulo';
+    const url = 'https://localhost/algo.jpg'
+
+
+    test('debe de mostrar el componente correctamente', () => {
+        
+        const wrapper = shallow( <GifGridItem title={ title } url={ url } />);
+        expect( wrapper ).toMatchSnapshot();
+
+    });
 })
